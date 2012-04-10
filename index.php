2523a77ccbe3ad1029ee7228caf5dcdc791d3fe6
Slavs2006 <Slavs2006@bk.ru>
2012-04-10 23:00:43 +0400

    second commit

---
+++ index.php
@@ -0,0 +1,112 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+<title>freeroom IFMO</title>
+<link href="favicon.ico" rel="icon" type="image/x-icon" />
+<link href="favicon.ico" rel="shortcut icon" type="image/x-icon" />
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+<meta name="description" content="Поиск свободной аудитории в ИТМО">
+<meta name="keywords" content="свободная, аудитория, итмо, свободная, пустой, класс, пустой, ifmo, ifmo, итмо, freeroom, free, room, freeroom">
+<meta name="title" content="freeroom IFMO">
+<meta name="resource-type" content="document">
+<meta name="copyright" content=" nikopie. copyright 2011">
+<link rel="stylesheet" type="text/css" href="css/style.css" />
+<link href="main_style_css.css" type="text/css" rel="stylesheet">
+<link type="text/css" href="jquery_ui/css/custom-theme/jquery-ui-1.8.16.custom.css" rel="stylesheet" />	
+<script type="text/javascript" src="jquery_ui/js/jquery-1.7.1.min.js"></script>
+<script type="text/javascript" src="jquery_ui/js/jquery-ui-1.8.18.custom.min.js"></script>
+<script type="text/javascript" src="./my_proj_script.js"></script>
+<script src="liveMap.js"></script>
+<link rel="stylesheet" type="text/css" href="css/buttons.css" />
+<link rel="stylesheet" type="text/css" href="css/shadow.css" />
+
+<style type="text/css">@import "css/jquery.datepick.css";</style> 
+<script type="text/javascript" src="js/jquery.datepick.js"></script>
+<script type="text/javascript" src="js/jquery.datepick.ext.js"></script>
+<script type="text/javascript" src="js/jquery.datepick-ru.js"></script>
+</head>
+<body>
+	<div id="dim">
+		<div id = "intro" class="msgbox">
+		<a class="close" href="#" ><img src="css/close.jpg"/></a>
+		 <IFRAME width="100%" height="100%"  name=frm id="frm" src="http://freeroom.et-tech.net"></IFRAME>
+			
+		</div>
+	</div>
+	
+		<div id="wrapper">
+		
+			<div id="menu2">FREE ROOM <br> PROJECT
+			</div>
+			<object data="shade.svg" type="image/svg+xml" class = "shade imap" id = "shade">
+				<p>К сожалению, вы используете устаревшую версию браузера, который не поддерживает интерактивную карту.</p>
+			</object>
+			<object data="1.svg" type="image/svg+xml" class = "imap" id = "1floorSVG">
+				<p>К сожалению, вы используете устаревшую версию браузера, который не поддерживает интерактивную карту.</p>
+			</object>
+			<object data="2.svg" type="image/svg+xml" class = "imap" id = "2floorSVG">
+				<p>К сожалению, вы используете устаревшую версию браузера, который не поддерживает интерактивную карту.</p>
+			</object>
+			<object data="3.svg" type="image/svg+xml" class = "imap" id = "3floorSVG">
+				<p>К сожалению, вы используете устаревшую версию браузера, который не поддерживает интерактивную карту.</p>
+			</object>
+			<object data="4.svg" type="image/svg+xml" class = "imap" id = "4floorSVG">
+				<p>К сожалению, вы используете устаревшую версию браузера, который не поддерживает интерактивную карту.</p>
+			</object>
+			<object data="5.svg" type="image/svg+xml" class = "imap" id = "5floorSVG">
+				<p>К сожалению, вы используете устаревшую версию браузера, который не поддерживает интерактивную карту.</p>
+			</object>
+			<object data="6.svg" type="image/svg+xml" class = "imap" id = "6floorSVG">
+				<p>К сожалению, вы используете устаревшую версию браузера, который не поддерживает интерактивную карту.</p>
+			</object>		
+			
+			<div class="timeset"> 
+				<div id="head">Выберите время и дату :</div>
+				<div id="calendar"></div>
+					<ul>
+						<li>
+						<a id="9" href="#">Весь день</a> 
+						</li> 
+						<li>
+						<a id="1" href="#">08 : 00 - 09 : 20</a> 
+						</li> 
+						<li>
+						<a id="2" href="#">09 : 30 - 10 : 50</a> 
+						</li> 
+						<li>
+						<a id="3" href="#">11 : 00 - 12 : 20</a> 
+						</li> 
+						<li>
+						<a id="4" href="#">12 : 40 - 14 : 00</a> 
+						</li> 
+						<li>
+						<a id="5" href="#">14 : 20 - 15 : 40</a> 
+						</li> 
+						<li>
+						<a id="6" href="#">15 : 50 - 17 : 10</a> 
+						</li> 
+						<li>
+						<a id="7" href="#">17 : 20 - 18 : 40</a> 
+						</li> 
+					</ul>	
+			</div>
+			<div class="block">
+			</div>
+			<div id="menu">
+				<a href="#" id="find" class="button orange small">Find freeroom</a>
+				<a href="#" id="enter" class="button orange small">Вход/регистрация</a>
+			</div>
+				
+			<div class="block"> 
+			</div>
+			
+		
+			
+		</div>
+			<div id="answerStruct">
+			</div>
+	
+
+
+</body>
+</html>
