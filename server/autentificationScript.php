37efd5b3d1ec4f67185615a85b29d8f1576662c1
NickTieck <nkapliev@gmail.com>
2012-06-19 01:55:01 +0400

    autentification whith switch on . session_start is ok..

--- server/autentificationScript.php
+++ server/autentificationScript.php
@@ -1,4 +1,5 @@
 <?php
+	session_start();
 	
 	function preprocessingOfLoginAndPass ($_login, $_pass) {
 		...// preprocessing input value and return status of checking. preprocessing = find taboo symbol   ^:)(*-_? etc.
@@ -6,26 +7,36 @@
 	}
 	function checkLoginAndPassInDB ($_login, $_pass) {	
 		...	//search in DB user with login == $login and pass == md5($pass). 
-		return $statusOfUser; // status from DB record of user: 1.dispatcher 2.simpleUser 3.invalid
+		return $statusOfUser; // status from DB record of user: 1.dispatcher 2.simpleUser 3.guest 4.invalid
 	}
 	
 	$login = $_POST['login'];
 	$pass = $_POST['pass'];
 	if (preprocessingOfLoginAndPass($login, $pass) == "notOK") {
-		echo "error";
+		session_destroy();
+		echo "error";// not simple error. must send file of registration with message of alert...???
 	}
 	else {
-		$statusOfUser = checkLoginAndPassInDB($login, $pass);	//in DB should also be kept user with login Guest, empty pass and status Guest 
+		$statusOfUser = checkLoginAndPassInDB($login, $pass);	//in DB should also be kept user with login Guest, empty pass and status guest 
 		if ($statusOfUser == 'invalid') {
-			echo "wrong data";
+			session_destroy();
+			echo "wrong data"; // same thing...???
 		}
 		else {
-			start_session();
 			$_SESSION['login'] = $login;
 			$_SESSION['status'] = $statusOfUser;
-			$_SESSION['controller'] = new UserMVC($statusOfUser);
-			echo $_SESSION['controller']->initUserInterfaceInHtml();	// how send new full page???? not Ajax answer...
+			switch ($statusOfUser) {
+				case 'dispatcher':	$_SESSION['controller'] = new DispatcherMVC;
+									break;
+				case 'simpleUser':	$_SESSION['controller'] = new SimpleUserMVC;
+									break;
+				case 'guest':		$_SESSION['controller'] = new GuestMVC;
+									break;
+				default:			session_destroy();
+									echo 'Who is this guy?'; // same thing....?????????
+									break;
+			}
+			echo $_SESSION['controller']->$viewOfUser->initUserInterfaceInHtml();
 		}
 	}
-	
 ?>
